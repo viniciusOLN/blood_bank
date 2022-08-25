99958aec813bac0d6260735a0b71c045e984fb6b
vinicius <vinicius853o@gmail.com>
2022-08-24 22:50:39 -0300

    styling modal bases component

--- src/components/modal/styled.js
+++ src/components/modal/styled.js
@@ -2,13 +2,27 @@ import styled from 'styled-components';
 
 const OverlayDiv = styled.div`
   width: 35%;
-  height: 80%;
   background-color: var(--background-white-color);
   border-radius: var(--border-radius);
   box-shadow: var(--modal-box-shadow);
   display: flex;
-  align-items:center;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
+  padding: 10px;
 `
 
-export {OverlayDiv}
+const CloseButtonDiv = styled.div`
+  width: 100%;
+  display: flex;
+  height: 30px;
+  justify-content: end;
+
+  i{
+    font-size: 30px;
+    cursor: pointer;
+    color: var(--title-black)
+  }
+`
+
+export {OverlayDiv, CloseButtonDiv}
