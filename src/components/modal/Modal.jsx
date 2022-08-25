520449165fb501c7f161cad812d83d0ced36f09c
vinicius <vinicius853o@gmail.com>
2022-08-24 22:50:23 -0300

    styling the modal bases component

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,11 +1,18 @@
-import { OverlayDiv } from "./styled";
+import { removeModal } from "../../utils/modalUtils";
+import { OverlayDiv, CloseButtonDiv } from "./styled";
 
 const Modal = (props) => {
-  return(
-    <OverlayDiv>
-      <div>
 
-      </div>
+  const handleCloseModal = () => {
+    const modal = document.getElementById("modal");
+    removeModal(modal);
+  }
+
+  return (
+    <OverlayDiv>
+      <CloseButtonDiv>
+        <i className="ri-close-line" onClick={handleCloseModal}></i>
+      </CloseButtonDiv>
       {props.children}
     </OverlayDiv>
   )
