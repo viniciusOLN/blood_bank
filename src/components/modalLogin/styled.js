e96efe58169c6f3c360dbb15709016cc005ca198
vinicius <vinicius853o@gmail.com>
2022-08-24 22:51:10 -0300

    styling modal login

---
+++ src/components/modalLogin/styled.js
@@ -0,0 +1,20 @@
+import styled from  'styled-components';
+
+const TitleLogin = styled.h2`
+  font-size: 30px;
+  color: var(--title-black);
+  font-weight: bold;
+  margin-bottom: 25px;
+`
+
+const BrandLogo = styled.div`
+  width: 50%;
+
+  img{
+     width: 100%;
+     height: 100%;
+  }
+`
+
+
+export { TitleLogin, BrandLogo }
