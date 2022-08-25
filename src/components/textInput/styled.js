c74d471c6bb9efc7ec6bc637eea13223a2205876
vinicius <vinicius853o@gmail.com>
2022-08-25 15:07:22 -0300

    adding the fixed style of label to text input

--- src/components/textInput/styled.js
+++ src/components/textInput/styled.js
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 const DivInput = styled.div`
   position: relative;
   &:focus-within #text{
-    transform: translate(0, -8px) scale(0.80);
+    transform: translate(0, -22px) scale(0.80);
   }
   margin-bottom: 10px;
+
 `
 
 const Input = styled.input `
@@ -23,9 +24,10 @@ const Placeholder = styled.div`
   background-color: var(--background-white-color);
   padding: 5px;
   position: absolute;
-  top: 0;
+  top: 12px;
   left: 8px;
-  transform: translate(0, 12px) scale(1);
+  // transform: translate(0, 12px) scale(1);
+  transform: ${(props)=> props.isActive ? 'translate(0, -22px) scale(0.80)' : ' transform: translate(0, -24px) scale(0.80)' };
   transform-origin: top left;
   transition: all 0.2s ease-out;
   pointer-events: none;
