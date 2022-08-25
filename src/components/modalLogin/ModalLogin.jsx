23cb2c6566f741c414ded0844dbd8d15746f8434
vinicius <vinicius853o@gmail.com>
2022-08-24 22:50:54 -0300

    starting to create modal login

--- src/components/modalLogin/ModalLogin.jsx
+++ src/components/modalLogin/ModalLogin.jsx
@@ -1,9 +1,17 @@
 import Modal from "../modal/Modal"
+import {TitleLogin, BrandLogo} from "./styled"
+import navbarIcon from '../../assets/images/navbar-icon.svg';
+import TextInput from "../textInput/TextInput";
 
 const ModalLogin = () => {
   return (
     <Modal title='adad'>
-      <h2>adadada</h2>
+     <TitleLogin>Login</TitleLogin>
+      <BrandLogo>
+        <img src={navbarIcon} alt="Blood Bank Logo System" title='Sistema de coleta de sangue'/>
+      </BrandLogo>
+      <TextInput placeholder='Email'/>
+      <TextInput placeholder='Senha'/>
     </Modal>
   )
 }
