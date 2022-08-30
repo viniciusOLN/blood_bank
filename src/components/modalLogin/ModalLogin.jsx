bd9a3bcb37f216a7b62ee1c0517cd68406966996
vinicius <vinicius853o@gmail.com>
2022-08-29 21:01:32 -0300

    adding show/hide password button

--- src/components/modalLogin/ModalLogin.jsx
+++ src/components/modalLogin/ModalLogin.jsx
@@ -9,23 +9,40 @@ const ModalLogin = () => {
   
   const [valueInputEmail, setValueInputEmail] = useState('')
   const [valueInputPassword, setValueInputPassword] = useState('')
+  const [hidePassword, setHidePassword] = useState(false);
 
+  
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(valueInputEmail)
     console.log(valueInputPassword)
   }
 
+  const handleHiddePassword = () => setHidePassword(!hidePassword)
+
   return (
     <Modal title='adad'>
      <TitleLogin>Login</TitleLogin>
       <BrandLogo>
-        <img src={navbarIcon} alt="Blood Bank Logo System" title='Sistema de coleta de sangue'/>
+        <img src={ navbarIcon } alt="Blood Bank Logo System" title='Sistema de coleta de sangue'/>
       </BrandLogo>
       <DivLogin>
-        <form onSubmit={handleSubmit}>
-          <TextInput value = {valueInputEmail} name='email' setValueInput = {setValueInputEmail} placeholder='Email'/>
-          <TextInput value = {valueInputPassword} name='password' setValueInput = {setValueInputPassword} placeholder='Senha'/>
+        <form onSubmit={ handleSubmit }>
+          <TextInput
+            value = { valueInputEmail } 
+            type='Email' 
+            name='email' 
+            setValueInput = { setValueInputEmail } 
+            placeholder='Email'
+          />    
+          <TextInput value = { valueInputPassword } 
+            type={ hidePassword ? 'Text' : 'Password' }
+            name='password'
+            icon ={ hidePassword ? 'ri-eye-off-line' : 'ri-eye-line' }
+            onClickIcon={ handleHiddePassword } 
+            setValueInput = { setValueInputPassword }
+            placeholder='Senha'
+          />
           <ButtonDefault title='Entrar' />
         </form>
         <CreateAccount>
